aa29b97ba6bdea183970ca455cf6fbbd0c04be0a
catprogrammer <hei411@gmail.com>
2021-02-23 17:03:54 +0000

    slight modifications. SV 3 still incomplete

--- Supervisionwork/SV3.thy
+++ Supervisionwork/SV3.thy
@@ -7,25 +7,27 @@ lemma"(\<forall>x. (P(x) \<longleftrightarrow> (Q(x) \<and> \<not>Q(f(x)))))\<lo
   done
 
 lemma"(\<forall>x. (P(x) \<longleftrightarrow> (Q(x) \<and> \<not>Q(f(x)))))\<longrightarrow> ( \<exists> y . \<not>P(y))"
-proof (rule impI)
-  fix a
+proof 
   assume condition:"(\<forall>x. (P(x) \<longleftrightarrow> (Q(x) \<and> \<not>Q(f(x)))))"
-  then have "(P(a) \<longleftrightarrow> (Q(a) \<and> \<not>Q(f(a))))" by simp
-  from condition have "\<exists> y . \<not>P(y)"
+  fix a
+  from condition have "(P(a) \<longleftrightarrow> (Q(a) \<and> \<not>Q(f(a))))" by simp
+  from condition have conclusion:"\<exists> y . \<not>P(y)"
   proof cases
     assume  "\<not> P(a)"
     then show ?thesis by auto
   next 
     assume q:" \<not> \<not> P a"
-    assume w: "P a = (Q a \<and> \<not> Q (f a))"
+    from condition have  w: "P a = (Q a \<and> \<not> Q (f a))" by simp
     from q have e:"P a" by simp
     from w e have "(Q a \<and> \<not> Q (f a))" by auto
     then have "\<not> Q (f a)" by simp
     then have t:"\<not> ( Q (f a)\<and>\<not>Q(f(f(a))))" by auto
     from condition t have y:"\<not>P(f(a))" by auto
-    finally show ?thesis
+    then show ?thesis by auto
   qed
   oops
+  
+  
     
 
 
